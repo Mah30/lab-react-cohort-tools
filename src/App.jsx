8c78ea47d458bc0a30d4ca9bc02061158860faa0
Mariah <alinemariah@hotmail.com>
2025-02-02 17:03:36 +0100

    bonus done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import studentsData from "./assets/students.json";
 
 import { Routes, Route} from "react-router-dom";
 
@@ -14,11 +15,15 @@ function App() {
       <Navbar />
 
       <div className="pages">
-        <HomePage />
 
-        <StudentDetailsPage />
+        <Routes>
+        <Route path='/' element={<HomePage />} />
 
-        <UserProfilePage />
+         <Route path='/students/:studentId' element={<StudentDetailsPage students={studentsData} />} />
+
+          <Route path='/profile' element={<UserProfilePage />} />
+          <Route path='*' element={<h1>404 <br /> page not found</h1>} />
+        </Routes>
       </div>
 
     </div>
